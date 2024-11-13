b5e1fccce06a6263692a04c6bfb7a42ff18f46f9
jpâmela_root <59771746+jpamela-root@users.noreply.github.com>
2024-11-13 11:24:50 -0300

    aula1

---
+++ src/main/java/org/gamespring/gamespring/GameSpringApplication.java
@@ -0,0 +1,13 @@
+package org.gamespring.gamespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gamespring/gamespring/GameSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gamespring.gamespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
